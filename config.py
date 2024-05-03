b84e1a8288f2abe99d23dbaefed297f0bcd5c05d
wiw <zmzhenya@yandex.ru>
2024-05-03 17:04:56 +0500

    тест

---
+++ config.py
@@ -0,0 +1,25 @@
+# Ваш IAM-токен и ссылку на аудиофайл в Object Storage.
+def get_iam_token_speeechkit():
+    key = '<>'
+    return key
+
+# Инициализация Telegram Bot
+def get_token_telegram():
+    token_bot = "<>" #тест local
+    return token_bot
+
+def get_user_id_telegram():
+    user_id_telegram = 1
+    return user_id_telegram
+
+def get_bucket_name_toBucketYAcloud():
+    bucket_name = '<>'
+    return bucket_name
+
+def get_aws_access_key_id():
+    aws_access_key_id = '<>'
+    return aws_access_key_id
+
+def get_aws_secret_access_key():
+    aws_secret_access_key = '<>'
+    return aws_secret_access_key
